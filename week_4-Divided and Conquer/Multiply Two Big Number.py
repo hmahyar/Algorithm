e59632c12ae603f7c8d3f6b740ebb002b513edd8
hmahyar <hooman.mahyar@gmail.com>
2016-03-26 21:16:27 -0700

    Add : Multiply Two Big Number

---
+++ week_4-Divided and Conquer/Multiply Two Big Number.py
@@ -0,0 +1,9 @@
+'''
+13*24 = (X+3)*(2X+4)
+C(X)  = (2X**2 + 10X + 12)
+C(10) = 2*10**2 + 10*10 + 12 #for each coefficient, from right to left subtract 10 and plus previous coefficient
+C(10) = 2*10**2 + 11*10 + 2
+C(10) = 3*10**2 + 1*10 + 2
+C(10) = 300 + 10 + 2
+C(10) = 312
+'''
