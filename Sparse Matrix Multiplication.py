04f57f99d9e1db7fa95762ca7292539c226920fe
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-08 01:48:55 -0800

    Update Sparse Matrix Multiplication.py
    Add Comment and O notation

--- Sparse Matrix Multiplication.py
+++ Sparse Matrix Multiplication.py
@@ -1,3 +1,30 @@
+'''
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+    ]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+    ]
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+
+If A(i*j) and B(j*k)
+Time = O(i*j*k) 
+Space = O(i*k)
+'''
+
 class Solution(object):
     def multiply(self, A, B):
     	result = [[0 for _ in xrange(len(B[0]))] for __ in xrange(len(A))]
