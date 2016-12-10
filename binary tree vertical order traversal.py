0a6916829c41a1bf1294f51eb961ab80e1ceed7c
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-09 17:13:22 -0800

    Add comment and Big O notation

--- binary tree vertical order traversal.py
+++ binary tree vertical order traversal.py
@@ -1,3 +1,34 @@
+'''
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+
+If two nodes are in the same row and column, the order should be from left to right.
+
+
+Given binary tree [3,9,8,4,0,1,7],
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7
+return its vertical order traversal as:
+[
+  [4],
+  [9],
+  [3,0,1],
+  [8],
+  [7]
+]
+
+
+Time=  O(n)
+Space= O(n)
+'''
+
+
+
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
