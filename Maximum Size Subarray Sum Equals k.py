4b29233771814bbe2fd9cb143e94e6d10bf8a957
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-13 22:25:28 -0800

    Fix a Bug

--- Maximum Size Subarray Sum Equals k.py
+++ Maximum Size Subarray Sum Equals k.py
@@ -12,8 +12,8 @@ class Solution(object):
         result  = 0
         for i in nums:
             curent_sum += i 
-            if current_sum ==k: result=i+1
-            if current_sum - k in sum_zero_to_i : resutl = max(resutl , i-sum_zero_to_i[(current_sum - k)])
+            if current_sum == k: result=i+1
+            elif current_sum - k in sum_zero_to_i : resutl = max(resutl , i-sum_zero_to_i[(current_sum - k)])
             if current_sum not in sum_zero_to_i:
                 sum_zero_to_i[current_sum] = i
             return result
