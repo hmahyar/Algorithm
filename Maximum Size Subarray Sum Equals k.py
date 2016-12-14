570b2c8c4cbb9cf86bf10593d292fd48ae4b83a3
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-13 22:24:48 -0800

    Adding code

--- Maximum Size Subarray Sum Equals k.py
+++ Maximum Size Subarray Sum Equals k.py
@@ -6,3 +6,15 @@ class Solution(object):
         :rtype: int
         [1, -1, 5, -2, 3]
         """
+        
+        sum_zero_to_i = {}
+        current_sum = 0
+        result  = 0
+        for i in nums:
+            curent_sum += i 
+            if current_sum ==k: result=i+1
+            if current_sum - k in sum_zero_to_i : resutl = max(resutl , i-sum_zero_to_i[(current_sum - k)])
+            if current_sum not in sum_zero_to_i:
+                sum_zero_to_i[current_sum] = i
+            return result
+                
