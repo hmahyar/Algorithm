b12856cfbb23f83b92b06b0fa7ceca82ff5d9e00
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-13 21:08:00 -0800

    Update add Binary.py

--- add Binary.py
+++ add Binary.py
@@ -1,30 +1,30 @@
-#create-maximum-number.py
-# Time:  O(k * (m + n + k)) ~ O(k * (m + n + k^2))
-# Space: O(m + n + k^2)
-#
-# Given two arrays of length m and n with digits 0-9 representing two numbers.
-# Create the maximum number of length k <= m + n from digits of the two.
-# The relative order of the digits from the same array must be preserved.
-# Return an array of the k digits. You should try to optimize your time
-# and space complexity.
-#
-# Example 1:
-# nums1 = [3, 4, 6, 5]
-# nums2 = [9, 1, 2, 5, 8, 3]
-# k = 5
-# return [9, 8, 6, 5, 3]
-#
-# Example 2:
-# nums1 = [6, 7]
-# nums2 = [6, 0, 4]
-# k = 5
-# return [6, 7, 6, 0, 4]
-#
-# Example 3:
-# nums1 = [3, 9]
-# nums2 = [8, 9]
-# k = 3
-# return [9, 8, 9]
-#
+'''
+Given two binary strings, return their sum (also a binary string).
 
-# DP + Greedy solution. (280ms)
+For example,
+a = "11"
+b = "1"
+Return "100".
+'''
+
+
+
+
+
+class Solution(object):
+	def addBinary(self, a, b):
+		"""
+		:type a: str
+		:type b: str
+		:rtype: str
+		"""
+		r,v,c = '',0,0
+		for i in xrange(0,max(len(a),len(b))):
+			if i<len(a): v += int(a[-(i+1)])
+			if i<len(b): v += int(b[-(i+1)])
+			v,c = v%2 , v//2 
+			r+=str(v)
+			v=c
+		if c!=0:
+			return (r+str(v))[::-1]
+		return r[::-1]
