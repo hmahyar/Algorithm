1a9b0d28fd6c9dc0e2c034f94b5851f07f419475
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-15 03:13:32 -0800

    Add Complexity

--- LeetCode/Letter Combinations of a Phone Number.py
+++ LeetCode/Letter Combinations of a Phone Number.py
@@ -1,3 +1,20 @@
+'''
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+
+
+Time= O(4n) ,n is number of digits in input number.
+'''
+
+
 class Solution:
     # @return a list of strings, [s1, s2]
     def __init__(self):
@@ -14,3 +31,5 @@ class Solution:
     	else:
     		for item in self.lookup[int(digits[lev])]:
     			self.Recu(digits,com+item,lev+1)
+
+                
