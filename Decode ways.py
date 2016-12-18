4e6c9b6c463e78bc9cac563216fe8ddd58e08cd7
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-17 21:56:18 -0800

    Create Decode ways.py

---
+++ Decode ways.py
@@ -0,0 +1,29 @@
+'''
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+'''
+
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s)==0 or s=='0': return 0
+        a,b = 1,0
+        for i in xrange(len(s)):
+            sum = 0
+            if s[i]!='0': sum=a
+            if i>0 and s[i-1]=='1' or (s[i-1]=='2' and s[i]<='6'): sum+=b
+            a,b=sum,a
+        return a
