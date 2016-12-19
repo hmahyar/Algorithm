9bbd42ce95be97f7e161140e2ef9d49d01b17b5e
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-18 19:24:36 -0800

    Create Find the Celebrity.py

---
+++ LeetCode/Find the Celebrity.py
@@ -0,0 +1,35 @@
+'''
+Total Accepted: 18632
+Total Submissions: 52620
+Difficulty: Medium
+Contributors: Admin
+Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+
+Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+
+You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
+
+Note: There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+
+Time = O(n)+O(n) = O(n)
+'''
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        c = 0
+        for p in xrange(1,n):
+            if knows(c,p):
+                c = p 
+        for p in xrange(n):
+            if p != c and knows(c,p) or not knows(p,c):
+                return -1
+        return c
