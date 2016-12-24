fe28d464f7552f057a802b64a3b54bdf298f4e72
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-24 02:28:59 -0800

    Update Split Array Largest Sum.py

--- LeetCode/Split Array Largest Sum.py
+++ LeetCode/Split Array Largest Sum.py
@@ -17,6 +17,17 @@ Explanation:
 There are four ways to split nums into two subarrays.
 The best way is to split it into [7,2,5] and [10,8],
 where the largest sum among the two subarrays is only 18.
+
+____________________________________________________________________
+Solution: 
+1) the maximum answer(biggest answer) regardless of input m is sum of the input nums, 
+and the minimum possibple answer regardless of input m is biggest number in nums list 
+2) checking a possible answer is easy. assume we have a candidate we just wanted to make sure weather it is working or not
+3) so this question it is a really simple bineary search, we have maximum possible answer and minumum possible answer
+we just need to apply a binary search to see if our candidate weather is valin or not 
+
+
+Time = O(nlogn), n= biggest pisiibility or sum of nums
 '''
 
 class Solution(object):
