6d6228a30a85f8011bd1b3d79c5228c56792122b
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-26 18:49:25 -0800

    Create longest Increasing Subsequence.py

---
+++ LeetCode/longest Increasing Subsequence.py
@@ -0,0 +1,23 @@
+'''
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+For example,
+Given [100, 4, 200, 1, 3, 2],
+The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+
+Your algorithm should run in O(n) complexity.
+'''
+
+class Solution2(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = []  # dp[i]: the length of LIS ends with nums[i]
+        for i in xrange(len(nums)):
+            dp.append(1)
+            for j in xrange(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp) if dp else 0
