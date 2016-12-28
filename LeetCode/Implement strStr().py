71b040982619ed30e295403b73de38013dedd150
Hooman Mahyar <hooman.mahyar@gmail.com>
2016-12-28 02:30:54 -0800

    Create Implement strStr().py

---
+++ LeetCode/Implement strStr().py
@@ -0,0 +1,18 @@
+'''
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+'''
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in xrange(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+            
