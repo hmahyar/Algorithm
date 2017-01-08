e5a3bfbd17ae150b28ee709b9c9c883c4c24c14e
hmahyar <hooman.mahyar@gmail.com>
2017-01-07 20:48:07 -0800

    Wild Card Matching

---
+++ LeetCode/Wildcard Matching.py
@@ -0,0 +1,41 @@
+'''
+Implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+'''
+
+
+
+class Solution():
+    # @return a boolean
+    def isMatch(self, s, p):
+        result = [[False for j in xrange(len(p) + 1)] for i in xrange(len(s) + 1)]
+        result[0][0] = True
+        
+        for i in xrange(1, len(p) + 1):
+            if p[i-1] == '*': result[0][i] = result[0][i-1] #*
+                    
+
+        for i in xrange(1,len(s) + 1):
+            for j in xrange(1, len(p) + 1):
+                if p[j-1] != '*':
+                    result[i][j] = result[i-1][j-1] and (s[i-1] == p[j-1] or p[j-1] == '?')
+                else:
+                    result[i][j] = result[i][j-1] or result[i-1][j] 
+          
+        return result[len(s)][len(p)]
