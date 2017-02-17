c52898108d1299251bcfe895327c6d8212e3ab0b
hmahyar <hooman.mahyar@gmail.com>
2017-02-16 23:30:33 -0800

    Update Solution

--- LeetCode/Regular Expression Matching.py
+++ LeetCode/Regular Expression Matching.py
@@ -1,48 +1,23 @@
-'''
-Implement regular expression matching with support for '.' and '*'.
-
-'.' Matches any single character.
-'*' Matches zero or more of the preceding element.
-
-The matching should cover the entire input string (not partial).
-
-The function prototype should be:
-bool isMatch(const char *s, const char *p)
-
-Some examples:
-isMatch("aa","a") → false
-isMatch("aa","aa") → true
-isMatch("aaa","aa") → false
-isMatch("aa", "a*") → true
-isMatch("aa", ".*") → true
-isMatch("ab", ".*") → true
-isMatch("aab", "c*a*b") → true
-'''
-
-
-class Solution():
-    # @return a boolean
+class Solution(object):
     def isMatch(self, s, p):
-        result = [[False for j in xrange(len(p) + 1)] for i in xrange(len(s) + 1)]
-        result[0][0] = True
-        for i in xrange(2, len(p) + 1):
-            if p[i-1] == '*': result[0][i] = result[0][i-2] #A8 ,A*B*, A*B*C*
-                    
-
-        for i in xrange(1,len(s) + 1):
-            for j in xrange(1, len(p) + 1):
-                if p[j-1] != '*':
-                    result[i][j] = result[i-1][j-1] and (s[i-1] == p[j-1] or p[j-1] == '.')
-                else:
-                    result[i][j] = result[i][j-1] or result[i-1][j]
-         
-        return result[len(s)][len(p)]
+        T = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        
+        T[0][0] = True
 
 
+        for j in xrange(1,len(T[0])):
+            if p[j-1]=='*': T[0][j] =T[0][j-2] 
 
 
+        for i in xrange(1,len(s)+1):
+            for j in xrange(1,len(p)+1):
+                
+                if p[j-1]==s[i-1] or p[j-1]=='.':
+                    T[i][j] = T[i-1][j-1]
+                
+                elif p[j-1]=='*':
+                    T[i][j] = T[i][j-2]
+                    if s[i-1]==p[j-2] or p[j-2]=='.':
+                        T[i][j] =T[i][j] | T[i-1][j]
 
-
-
-#if __name__ == "__main__":   
-#    print Solution().isMatch('ABBC','AB*C')
+        return T[-1][-1]
